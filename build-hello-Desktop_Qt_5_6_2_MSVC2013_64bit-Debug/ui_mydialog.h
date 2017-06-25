7fc0304e118f6cab7a5ebf23046d54ec0ba8e7d4
sdmengxiangyu <sdmengxiangyu@gmail.com>
2017-06-25 20:12:05 +0800

    hello in qt

---
+++ build-hello-Desktop_Qt_5_6_2_MSVC2013_64bit-Debug/ui_mydialog.h
@@ -0,0 +1,55 @@
+/********************************************************************************
+** Form generated from reading UI file 'mydialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.6.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MYDIALOG_H
+#define UI_MYDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MyDialog
+{
+public:
+    QLabel *label;
+
+    void setupUi(QDialog *MyDialog)
+    {
+        if (MyDialog->objectName().isEmpty())
+            MyDialog->setObjectName(QStringLiteral("MyDialog"));
+        MyDialog->resize(515, 350);
+        label = new QLabel(MyDialog);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(150, 110, 71, 31));
+
+        retranslateUi(MyDialog);
+
+        QMetaObject::connectSlotsByName(MyDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *MyDialog)
+    {
+        MyDialog->setWindowTitle(QApplication::translate("MyDialog", "MyDialog", 0));
+        label->setText(QApplication::translate("MyDialog", "hello world", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MyDialog: public Ui_MyDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MYDIALOG_H
