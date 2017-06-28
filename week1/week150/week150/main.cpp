30d462b488f02a42df363e467415e6418243b04c
sdmengxiangyu <sdmengxiangyu@gmail.com>
2017-06-28 11:16:38 +0800

    链表结束

--- week1/week150/week150/main.cpp
+++ week1/week150/week150/main.cpp
@@ -1,91 +1,58 @@
+
+/*．数制的转换
+把一个10进制的整数m(0<m< 216)转换成n进制数（n = 2, 8或16 ），在主函数中输入m和n值，在主函数中输出转换后的序列 。
+1）用非递归算法实现
+2）用递归算法实现
+*/
+
 #include<iostream>
 using namespace std;
-void func(int m, int n){
-	int i = 0, j;
-	int b[32];
-	switch (n)
-	{
-	case 2:{
-			   if (m > 0){
-				   b[i++] = m % 2;
-				   m /= 2;
-				   func(m, n);
-			   }
-			   b[i] = '\n';
-			   for (j = i - 1; j >= 0; j--)
-				   cout << b[j];
-	}
-	case 8:{
-			   if (m > 0){
-				   b[i++] = m % 8;
-				   m /= 8;
-				   func(m, n);
-			   }
-			   b[i] = '\n';
-			   for (j = i - 1; j >= 0; j--)
-				   cout << b[j];
-	}
-	case 16:{
-			   if (m > 0){
-				   b[i++] = m % 16;
-				   m /= 16;
-				   func(m, n);
-			   }
-			   b[i] = '\n';
-			   for (j = i - 1; j >= 0; j--)
-				   cout << b[j];
-	}
 
-	default:
-		break;
+int func1(int n, int p,int *s){
+	int c, m = 0;
+	while (n != 0){
+		c = n%p;	//c是余数
+		n = n / p;
+		m++;
+		s[m] = c;
 	}
+	return m;
+}
+
+int num[100];
+int number = 0;
+int func2(int n, int p){
+	int a, b;
+	a = n%p;
+	b = n / p;
+	num[number++] = a;
+	if (b != 0)
+		return func2(b, p);	//递归算法
+	else return 0;
 }
 
 int main(){
-	int m, n;
-	cout << "输入十进制数m(1<m<216)，转化的进制n（2,8,16）\n";
-	cin >> m >> n;
-	switch (n)
-	{
-	case 2:{
-			   int i = 0, j;
-			   int b[32];
-			   while (m > 0){
-				   b[i++] = m % 2;
-				   m /= 2;
-			   }
-			   for (j = i - 1; j >= 0; j--)
-				   cout << b[j];
-			   cout << endl;
+	int n, p, s[100];
+	cout << "输入转换的数字和进制\n";
+	cin >> n >> p;
+	int m = func1(n, p, s);
+	for (int k = m; k >= 1; k--){
+		if (s[k] >= 10)		//十六进制转化成字符.
+			cout << (char)(s[k] + 55);
+		else
+			cout << s[k];
 	}
-	case 8:{
-			   int i = 0, j;
-			   int b[32];
-			   while (m > 0){
-				   b[i++] = m % 8;
-				   m /= 8;
-			   }
-			   for (j = i - 1; j >= 0; j--)
-				   cout << b[j];
-			   cout << endl;
+	cout << endl;
+	func2(n, p);
+	for (int i = number - 1; i >= 0; i--){
+		if (num[i] >= 10)		//十六进制转化成字符.
+			cout << (char)(num[i] + 55);
+		else
+			cout << num[i];
 	}
-	case 16:{
-			   int i = 0, j;
-			   int b[32];
-			   while (m > 0){
-				   b[i++] = m % 16;
-				   m /= 16;
-			   }
-			   for (j = i - 1; j >= 0; j--)
-				   cout << b[j];
-			   cout << endl;
-	}
-	default:
-		break;
-	}
-	/////
-	cout << "输入\n";
-	cin >> m >> n;
-	func(m, n);
+	cout << endl;
 	return 0;
 }
+
+//递归ttp://blog.csdn.net/longzaitianya1989/article/details/8117718
+//转任意进制ttp://blog.csdn.net/xiaofei2010/article/details/7434737
