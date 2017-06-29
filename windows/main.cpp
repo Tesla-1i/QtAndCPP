81684ccb0993e1167c743bed55040c006e521c5d
sdmengxiangyu <sdmengxiangyu@gmail.com>
2017-06-29 17:34:26 +0800

    修改Readme

--- windows/main.cpp
+++ windows/main.cpp
@@ -1,9 +1,12 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include <QApplication>
 #include<iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+    for(int i=0;i<5;i++)
+        cout<<"你好\n";
     std::cout<<"你好啊\n";
     QApplication a(argc, argv);
     MainWindow w;
